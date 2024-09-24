08963941b5d66336260dece03c3d47b12dffc895
slee_ap <lessandro.fleite@gmail.com>
2024-09-24 17:59:45 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import ConteudoLateral from './componentes/ConteudoLateral'
+
+import './App.css';
+
+export default function App() {
+    return (
+        <div className="App">
+            <ConteudoLateral />
+            <div className="ConteudoPrincipal">
+                Conteudo Principal
+            </div>
+
+        </div>
+    );
+}
---
+++ src/componentes/ConteudoLateral.js
@@ -0,0 +1,15 @@
+export default function ConteudoLateral() {
+    return (
+        <div className="ConteudoLateral">
+            <div>Logo</div>
+            <div>Menu
+                <ul>
+                    <li>Home</li>
+                    <li>Pesquisa</li>
+                    <li>Explorar</li>
+                    <li>Mensagens</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
